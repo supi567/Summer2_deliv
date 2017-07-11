51bb078197dd0f5621fc61fdd9ca4e9251180139
karthikavjr2017 <avjrkarthik@gmail.com>
2017-07-11 17:21:31 -0400

    Rough version ; to be enhanced

--- Assignment1/Part2/MedicalDiag.c
+++ Assignment1/Part2/MedicalDiag.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+char getCase(char);
+int main()
+{
+	char symptom[100];
+	printf("enter any case symptom: ");
+	scanf("%c",&symptom);
+	
+	getCase(symptom);
+	
+	return 0;
+}
 
+char getCase(char symptom){
+	if(symptom='Fever'||'cough'||'runny'||'muscle')
+	{
+		printf("\n The entered Symptom belongs to :  Influenza");
+	}
+}
