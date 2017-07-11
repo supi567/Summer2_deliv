3e4d58470026df8e4101759ca032f5ef6114618a
karthikavjr2017 <avjrkarthik@gmail.com>
2017-07-11 15:45:20 -0400

    Rough working version
    Prolog program

--- Assignment1/Part1/BubbleSort.pl
+++ Assignment1/Part1/BubbleSort.pl
@@ -1,1 +1,16 @@
+% Bubble sort Algorithm in prolog
 
+bubble_sort([],Sorted) :-
+	Sorted = [].
+bubble_sort([X], Sorted) :-
+	Sorted = [X].
+bubble_sort(Terms, Sorted) :-
+	bubble(Terms, Terms), Sorted = Terms ;
+	bubble(Terms, Partials), bubble_sort(Partials, Sorted).
+
+bubble([], Bubbled) :- Bubbled = [].
+bubble([X], Bubbled) :- Bubbled = [X].
+bubble([X,Y|Terms], [Y|Bubbled]) :-
+	Y < X, bubble([X|Terms], Bubbled).
+bubble([X,Y|Terms], [X|Bubbled]) :-
+	X =< Y, bubble([Y|Terms], Bubbled).
